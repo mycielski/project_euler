57c2d16731418ec9d7fac2b1a8f7fdaf3e016c6c
mycielski <mycielski00@gmail.com>
2022-12-25 22:26:58 +0100

    feat: problem 17

---
+++ problems/number_letter_counts/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+from num2words import num2words
+
+
+def num_to_word(number: int) -> str:
+    return num2words(number).replace(" ", "").replace("-", "")
+
+
+if __name__ == "__main__":
+    count = 0
+    for i in range(int(sys.argv[1])):
+        count += len(num_to_word(i + 1))
+    print(count)
+    
